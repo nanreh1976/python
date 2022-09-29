3c7be8df0cb249d0d2a2587603c6d536cf617edc
Hernan Gomez <95485080+nanreh1976@users.noreply.github.com>
2022-09-29 11:24:56 -0300

    Inputs y Outputs

---
+++ python.py
@@ -0,0 +1,28 @@
+#inputs y outputs
+
+from operator import truediv
+
+
+respuestaUsuario = int (input("ingresa tu edad: ")) #input
+
+print("tu edad el año pasado fue: ", respuestaUsuario-1) #output
+
+print(f"tu edad el es: {respuestaUsuario}" ) #output
+
+print (f""" Tu edad actual 
+es: {respuestaUsuario}""") #output
+
+
+#Tipos de datos y operadores
+
+cadena1 = "2+4" #string
+cadena2= "milenapeletay@gmail.com" #string
+entero1 = 4 #int
+entero2 = 2+4 #int
+entero3 = -1 #int
+decimal1 = 1.5 #float
+decimal2 = 2.44 #float
+bool1 = True #boolean
+bool2 = False #boolean
+
+print(cadena1 + cadena2 + str(entero1) )
